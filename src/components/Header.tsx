e1c4aaf12a1a37bbfe963e8ea23005e3738ee97e
João victor morais mendes <114888951+codingwithvm@users.noreply.github.com>
2024-08-17 14:31:18 +0000

    feat: header complete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,16 @@
+import Logo from "./Logo";
+
 export default function Header() {
     return (
-        <header>
-            header
+        <header className="flex font-['Quicksand'] justify-around items-center py-4">
+            <Logo />
+            <ul className="flex gap-4">
+                <li><a href="#inicio">Início</a></li>
+                <li><a href="#projetos">Projetos</a></li>
+                <li><a href="#cursos">Cursos</a></li>
+                <li><a href="#certificacoes">Certificações</a></li>
+                <li><a href="#redessociais">Redes Sociais</a></li>
+            </ul>
         </header>
     )
 }
