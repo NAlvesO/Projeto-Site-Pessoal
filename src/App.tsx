d65bb7fbe7854251aaadca8816fcababb53069a1
daviiisousa <sousadavi248@gmail.com>
2024-08-17 11:53:56 -0300

    criei a seção de certificados da pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Certificacao } from "./components/certificacao";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+        <Certificacao />
+    </div>
   )
 }
