d6d76cf612c897d041761185f90c3b8d59a5f93f
Daniel Sampaio <danielsampaio127@gmail.com>
2024-08-18 22:02:50 -0300

    Arrumando layout e adicionando resposividade

---
+++ src/components/Main/icons/BootsTrap.tsx
@@ -0,0 +1,37 @@
+export default function BootsTrap() {
+    return (
+        <>
+            <svg className="absolute max-sm:w-24 w-32" viewBox="0 0 131 131" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_537_66)">
+                    <path d="M100.297 9.03869e-09L30.7031 2.76695e-09C13.7463 1.2388e-09 -1.2388e-09 13.7463 -2.76695e-09 30.7031L-9.03869e-09 100.297C-1.05668e-08 117.254 13.7463 131 30.7031 131L100.297 131C117.254 131 131 117.254 131 100.297L131 30.7031C131 13.7463 117.254 1.05668e-08 100.297 9.03869e-09Z" fill="url(#paint0_linear_537_66)" />
+                    <g filter="url(#filter0_d_537_66)">
+                        <path d="M67.5315 100.377C82.7173 100.377 91.8684 92.9414 91.8684 80.6776C91.8684 71.4068 85.3393 64.6956 75.6438 63.6327L75.6438 63.2469C82.7669 62.0879 88.3564 55.4729 88.3564 48.0857C88.3564 37.5596 80.0466 30.7031 67.3831 30.7031L38.8906 30.7031L38.8906 100.377L67.5315 100.377ZM49.9709 39.539L64.712 39.539C72.7255 39.539 77.2762 43.1123 77.2762 49.5825C77.2762 56.4871 71.9835 60.3496 62.3872 60.3496L49.9709 60.3496L49.9709 39.539ZM49.9709 91.5414L49.9709 68.6066L64.6127 68.6066C75.0998 68.6066 80.541 72.4691 80.541 80.0016C80.541 87.5336 75.2482 91.5414 65.2559 91.5414L49.9709 91.5414Z" fill="url(#paint1_linear_537_66)" />
+                        <path d="M67.5315 100.377C82.7173 100.377 91.8684 92.9414 91.8684 80.6776C91.8684 71.4068 85.3393 64.6956 75.6438 63.6327L75.6438 63.2469C82.7669 62.0879 88.3564 55.4729 88.3564 48.0857C88.3564 37.5596 80.0466 30.7031 67.3831 30.7031L38.8906 30.7031L38.8906 100.377L67.5315 100.377ZM49.9709 39.539L64.712 39.539C72.7255 39.539 77.2762 43.1123 77.2762 49.5825C77.2762 56.4871 71.9835 60.3495 62.3872 60.3495L49.9709 60.3495L49.9709 39.539ZM49.9709 91.5414L49.9709 68.6066L64.6127 68.6066C75.0998 68.6066 80.541 72.4691 80.541 80.0016C80.541 87.5336 75.2482 91.5414 65.2559 91.5414L49.9709 91.5414Z" stroke="white" stroke-width="2" />
+                    </g>
+                </g>
+                <defs>
+                    <filter id="filter0_d_537_66" x="21.8906" y="17.7031" width="86.9777" height="103.674" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                        <feOffset dy="4" />
+                        <feGaussianBlur stdDeviation="8" />
+                        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0" />
+                        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_537_66" />
+                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_537_66" result="shape" />
+                    </filter>
+                    <linearGradient id="paint0_linear_537_66" x1="0" y1="0" x2="131" y2="131" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="#9013FE" />
+                        <stop offset="1" stop-color="#6B11F4" />
+                    </linearGradient>
+                    <linearGradient id="paint1_linear_537_66" x1="43.9019" y1="35.2891" x2="76.0112" y2="89.5938" gradientUnits="userSpaceOnUse">
+                        <stop stop-color="white" />
+                        <stop offset="1" stop-color="#F1E5FC" />
+                    </linearGradient>
+                    <clipPath id="clip0_537_66">
+                        <rect width="131" height="131" fill="white" />
+                    </clipPath>
+                </defs>
+            </svg>
+        </>
+    )
+}
